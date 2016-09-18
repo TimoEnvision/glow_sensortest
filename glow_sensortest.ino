c989dbbdd2e2526aa5e40eb0e2a9f650fbbea31a
John Simons <jammsimons@gmail.com>
2016-09-18 15:07:20 +0200

    Some more splitting out into separate classes

--- glow_sensortest.ino
+++ glow_sensortest.ino
@@ -1,22 +1,4 @@
-/* IMPORTANT SIDENOTE
- *  Schematics show that antenna 1 should be connected to pin 25, pin 2 to 26 etc.
- *  In "real life" they are routed to the opposite pins AND the pins are mirrored!
- *  They are mirrored because the antenna 'S4' runs to MTI0 on the chip, S3 to MTI1, S2 to MTI3, S1 to MTI4
- *  So antenna 1 now routes to pin 14, antenna 2 to pin 15, 3 = 16 and 4 = 17. 
- */
-
-#include "FastLED.h"
-#include "sensors.cpp";
-// LED stuff
-
-// How many leds are in the strip?
-#define numLeds 112
-
-// Data pin that led data will be written out over
-#define DATA_PIN 7
-
-// This is an array of leds.  One item for each led in your strip.
-CRGB leds[numLeds];
+#include "Sensors.h";
 
 Sensors sensors;
 
@@ -30,6 +12,8 @@ void setup() {
   Serial.println(F("Adafruit MQTT demo"));
 
   sensors = new Sensors();
+
+  leds = new Leds();
 
   // Initialise the Client
   Serial.print(F("\nInit the Client..."));
--- leds.cpp
+++ leds.cpp
@@ -1,23 +1,22 @@
-#include <Arduino.h>
+#include "Arduino.h"
+#include "Leds.h"
 
-class Leds {
+Leds::Leds() {
+  FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, numLeds);
 
-  Leds() {
-    FastLED.addLeds<WS2812B, DATA_PIN, GRB>(leds, numLeds);
+  FastLED.setMaxPowerInVoltsAndMilliamps(5,5000);
+}
 
-    FastLED.setMaxPowerInVoltsAndMilliamps(5,5000);
-  }
-
-  void lightFromPCB() {
+Leds::lightFromPCB() {
     for(int i = 0; i < numLeds; i ++){
       leds[i] = CRGB::Red;
       //delay(6);
       FastLED.show();
       fadeToBlackBy(leds,numLeds,25);
     }
-  }
-  
-  void lightFromMid() {
+}
+
+Leds::lightFromMid() {
     for(int i = 0; i < numLeds; i ++){
       leds[numLeds/2 - i/2] = CRGB::Red;
       leds[numLeds/2 + i/2] = CRGB::Red;
@@ -25,17 +24,18 @@ class Leds {
       FastLED.show();
       fadeToBlackBy(leds,numLeds,25);
     }
-  }
-  void lightFromFarSide() {
+}
+  
+Leds::lightFromFarSide() {
     for(int i = 0; i < numLeds; i ++){
       leds[numLeds - i] = CRGB::Red;
       //delay(6);
       FastLED.show();
       fadeToBlackBy(leds,numLeds,25);
     }
-  }
+}
 
-  void update() {
+Leds::update() {
     switch(touchState){
     case 0:
       fadeToBlackBy(leds,numLeds,5);
@@ -55,5 +55,4 @@ class Leds {
       //delay(1000);
       break;
     }
-  }
 }
